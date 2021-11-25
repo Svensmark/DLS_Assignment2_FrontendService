df1c655110e82483d0572dd02903c8440b871b86
Emil <emil.svensmark@gmail.com>
2021-11-25 11:34:47 +0100

    Fixed functionality to send unique ID with token

--- src/login/LoggedIn.js
+++ src/login/LoggedIn.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from 'react';
 import facade from './ApiFacade'
 
 export default function LoggedIn() {
-    const [username, setUsername] = useState();
-    const [role, setRole] = useState();
+  const [id, setId] = useState();
+  const [username, setUsername] = useState();
+  const [role, setRole] = useState();
 
-    useEffect(() => {
-        facade.fetchUser().then(res => {setUsername(res.userName); setRole(res.roleList)}).catch(e => console.log(e));
-      }, [])
+  useEffect(() => {
+    facade.fetchUser().then(res => {
+      setId(res.id);
+      setUsername(res.userName);
+      setRole(res.roleList)
+    }).catch(e => console.log(e));
+  }, [])
 
-    return (
-        <div>
-          <h2>Data Received from server</h2>
-          <h3>Username: {username}</h3>
-          <h3>Role: {role}</h3>
-        </div>
-      )   
+  return (
+    <div>
+      <h2>Data Received from server</h2>
+      <h3>Unique id: {id}</h3>
+      <h3>Username: {username}</h3>
+      <h3>Role: {role}</h3>
+    </div>
+  )
 }
